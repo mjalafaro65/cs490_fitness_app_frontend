f4069a316aaaae6514afd86a85c36f5629cd366c
chocolate-juice <140909474+chocolate-juice@users.noreply.github.com>
2026-04-16 15:36:02 -0400

    changes to admin account

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ function Navbar() {
             to: "/coach/progresslogs", label: "Progress Logs", icon:(Icons.Logs)
         },
         {
-            to: "/coach/meallogs", label: "Meal Logs", icon: (Icons.Logs)
+            to: "/coach/meallogs", label: "Meal Logs", icon: (Icons.Meal)
         },
         {
             to: "/coach/settings", label: "Settings", icon: (Icons.Settings)
@@ -112,20 +112,16 @@ function Navbar() {
         {
             to: "/admin/workouts", label: "Workouts", icon: (Icons.Workout)
         },
-        {
-             to: "/admin/progresslogs", label: "Progress Logs", icon:(Icons.Logs)
-         },
+        //{
+        //     to: "/admin/progresslogs", label: "Progress Logs", icon:(Icons.Logs)
+        // },
         // {
-        //     to: "/admin/meallogs", label: "Meal Logs", icon: (
-        //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4">
-        //             <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-        //             <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-        //         </svg>
+        //     to: "/admin/meallogs", label: "Meals", icon: (Icons.Meal)
         //     )
         // },
-        {
-            to: "/admin/settings", label: "Settings", icon: (Icons.Settings)
-        },
+        //{
+        //    to: "/admin/settings", label: "Settings", icon: (Icons.Settings)
+        //},
     ];
 
     let allPages = [];
