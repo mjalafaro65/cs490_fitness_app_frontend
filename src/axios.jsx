aadca896c805ae293543c36c46df17adda68f4d2
Mjacm15 <alfamar2005@gmail.com>
2026-04-24 16:45:18 -0400

    fixe

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://nmzn3h98-5000.use.devtunnels.ms/" || "http://127.0.0.1:5000",
+  baseURL: "http://127.0.0.1:5000" ,
 });
+
+// const api = axios.create({
+//   baseURL: "https://nmzn3h98-5000.use.devtunnels.ms/" ,
+// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
